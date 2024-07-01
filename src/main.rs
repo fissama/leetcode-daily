fe84adf8688d1b6ac16d244851868a05d9192cc4
laphi1612 <laphi1612@gmail.com>
2024-07-01 06:27:52 +0000

    feat: complete daily 20240701

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+pub mod solution;
+
+include!("./solution.rs");
+
 fn main() {
-    println!("Hello, leetcode!");
+    let test1 = vec![2, 6, 4, 1];
+    let test2: Vec<i32> = vec![1,2,34,3,4,5,7,23,12];
+    let result = Solution::three_consecutive_odds(test1);
+    println!("Result test 1: {}", result);
+
+    let result = Solution::three_consecutive_odds(test2);
+    println!("Result test 2: {}", result);
 }
